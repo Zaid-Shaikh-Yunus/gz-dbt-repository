0b0d242f0f680d83889d07508a4c5e3876dab922
Zaid <z_95@hotmail.fr>
2026-02-04 18:14:32 +0000

    erreur lors du build essai si ok

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -7,7 +7,8 @@ WITH base AS (
         m.purchase_cost,
         m.margin AS operational_margin,
         s.shipping_fee,
-        s.log_cost
+        s.log_cost,
+        s.ship_cost
     FROM {{ ref('int_orders_operational') }} o
     LEFT JOIN {{ ref('int_orders_margin') }} m
         USING (orders_id, date_date)
@@ -24,6 +25,7 @@ SELECT
     SUM(purchase_cost) AS total_purchase_cost,
     SUM(shipping_fee) AS total_shipping_fees,
     SUM(log_cost) AS total_log_costs,
+    SUM(ship_cost) AS total_ship_costs,
     SUM(quantity) AS total_quantity_sold
 FROM base
 GROUP BY date_date
