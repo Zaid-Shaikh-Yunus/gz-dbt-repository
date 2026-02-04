eb271b628bdd8cb3fc6f2aa8faa33c9b9b440065
Zaid <z_95@hotmail.fr>
2026-02-04 16:26:04 +0000

    ajout de purchase_cost et margin

--- models/int_sales_margin.sql
+++ models/int_sales_margin.sql
@@ -1,4 +1,12 @@
-SELECT *
-FROM {{ ref('stg_gz_raw_data__sales') }}
-LEFT JOIN {{ ref('stg_gz_raw_data__product') }}
+SELECT
+    s.products_id,
+    s.orders_id,
+    s.date_date,
+    s.quantity,
+    s.revenue,
+    p.purchase_price,
+    s.quantity * p.purchase_price AS purchase_cost,
+    s.revenue - (s.quantity * p.purchase_price) AS margin
+FROM {{ ref('stg_gz_raw_data__sales') }} s
+LEFT JOIN {{ ref('stg_gz_raw_data__product') }} p
 USING (products_id);
