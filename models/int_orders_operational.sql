097b59cb3f87b0a86b60c705767dd7f3942280c1
Zaid-Asif Shaikh Yunus <z_95@hotmail.fr>
2026-02-05 08:40:20 +0000

    commit operational and finance

--- models/int_orders_operational.sql
+++ models/int_orders_operational.sql
@@ -1,21 +1,15 @@
-WITH shipping AS (
-    SELECT
-        orders_id,
-        CAST(shipping_fee AS FLOAT64) AS shipping_fee,
-        CAST(logCost AS FLOAT64) AS log_cost,
-        CAST(ship_cost AS FLOAT64) AS ship_cost
-    FROM {{ source('gz_raw_data', 'ship') }}
-),
-
-orders AS (
-    SELECT *
-    FROM {{ ref('int_orders_margin') }}
-)
-
-SELECT
-    orders_id,
-    date_date,
-    margin + shipping_fee - log_cost - ship_cost AS operational_margin
-FROM orders
-LEFT JOIN shipping
-USING (orders_id)
+ SELECT
+     o.orders_id
+     ,o.date_date
+     ,ROUND(o.margin + s.shipping_fee - (s.log_cost + s.ship_cost),2) AS operational_margin
+     ,o.quantity
+     ,o.revenue
+     ,o.purchase_cost
+     ,o.margin
+     ,s.shipping_fee
+     ,s.log_cost
+     ,s.ship_cost
+ FROM {{ref("int_orders_margin")}} o
+ LEFT JOIN {{ref("stg_gz_raw_data__ship")}} s
+     USING(orders_id)
+ ORDER BY orders_id desc
